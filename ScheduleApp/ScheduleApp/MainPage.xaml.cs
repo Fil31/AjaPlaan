e681982607590fde395c51a66a37d77795c61d62
Fil31 <101242957+Fil31@users.noreply.github.com>
2023-04-05 14:03:08 +0300

    Fixed

--- ScheduleApp/ScheduleApp/MainPage.xaml.cs
+++ ScheduleApp/ScheduleApp/MainPage.xaml.cs
@@ -21,9 +21,17 @@ namespace ScheduleApp
 
             ScheduleItems = new ObservableCollection<ScheduleItem>();
 
-            // Пример добавления элементов в расписание
-            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "08:00", Activity = "Завтрак", Description = "Приготовить омлет с сыром и томатами", IsChecked = false });
-            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "10:00", Activity = "Работа", Description = "Отправить отчет о проделанной работе", IsChecked = false });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "08:00", Activity = "Завтрак", Description = "Приготовить омлет с сыром и томатами" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "10:00", Activity = "Работа", Description = "Отправить отчет о проделанной работе" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "12:00", Activity = "Обед", Description = "Приготовить салат из свежих овощей" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "14:00", Activity = "Работа", Description = "Провести встречу с клиентом" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "16:00", Activity = "Занятия", Description = "Посетить занятия по английскому языку" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "18:00", Activity = "Ужин", Description = "Приготовить суп из свежих овощей" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "20:00", Activity = "Работа", Description = "Провести встречу с клиентом" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 4, Time = "22:00", Activity = "Сон", Description = "Провести ночь в спокойствии" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 7, Time = "14:00", Activity = "Работа", Description = "Провести встречу с клиентом" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 15, Time = "14:00", Activity = "Работа", Description = "Провести встречу с клиентом" });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = 23, Time = "14:00", Activity = "Работа", Description = "Провести встречу с клиентом"});
 
             scheduleListView.ItemsSource = ScheduleItems;
         }
@@ -44,7 +52,7 @@ namespace ScheduleApp
             string activity = await DisplayPromptAsync("Добавить дело", "Введите название дела");
             string description = await DisplayPromptAsync("Добавить дело", "Введите описание дела");
 
-            ScheduleItems.Add(new ScheduleItem { DayNumber = dayNumber, Time = time, Activity = activity, Description = description, IsChecked = false });
+            ScheduleItems.Add(new ScheduleItem { DayNumber = dayNumber, Time = time, Activity = activity, Description = description});
         }
 
 
@@ -54,7 +62,6 @@ namespace ScheduleApp
             public string Time { get; set; }
             public string Activity { get; set; }
             public string Description { get; set; }
-            public bool IsChecked { get; set; }
         }
     }
 }
